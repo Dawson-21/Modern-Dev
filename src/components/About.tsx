866235fccb7b57fc9202d9945976a2a7b1fc5c18
Dawson Adams <dawson.adams121@gmail.com>
2025-05-18 22:37:29 -0500

    Update spacing and add image for about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,32 +1,32 @@
 export default function About() {
-    return (
-        <div className="mt-[26vh] w-full h-fit bg-black/70 backdrop-blur-lg">
-            <div className="flex flex-col items-left justify-center px-4 sm:px-[10%] lg:px-[20%]">
-                <h1 className="text-3xl md:text-4xl  lg:text-5xl font-bold text-white mt-32">Welcome, I'm Dawson.</h1>
-                <p className="text-lg md:text-xl lg:text-2xl text-white mt-5">Web developer dedicated to helping small businesses build a stronger online presence and stand out in today's digital landscape.</p>
-                <div className="flex lg:justify-end mt-5 pr-5">
-                    <button className="flex group w-fit bg-white/80 text-black px-4 py-2 rounded-full text-md sm:text-lg font-bold hover:bg-white transform hover:scale-[1.15] transition duration-200 ease-in-out">
-                        Start a Project <img src="/arrow-black.svg" className="w-8 pl-1 transform group-hover:translate-x-3 transition duration-200 ease-in-out"/>
-                    </button>
-                </div>
-
-                <div className="flex flex-row w-full items-center justify-left mt-10">
-                    <p className="text-sm sm:text-lg text-white/55 w-24 md:w-28">About Me</p>
-                    <hr className="w-full border-t-1 border-white/15 mt-4 mb-3"/>
-                </div>
-
-                <div className="flex  flex-col md:flex-row space-x-10 pb-20">
-                    <div className="bg-black/90 rounded-xl border-white/15 border-1 text-white p-10 mt-10 w-full md:w-1/2">
-                        <h2 className="text-3xl font-bold">About Me</h2>
-                        <p className="text-lg mt-4">I am a passionate developer with a love for creating modern web applications. I specialize in React and Tailwind CSS.</p>
-                    </div>
-
-                    <div className="bg-black/90 rounded-xl border-white/15 border-1 text-white p-10 mt-10 w-full md:w-1/2">
-                        <h2 className="text-3xl font-bold">About Me</h2>
-                        <p className="text-lg mt-4">I am a passionate developer with a love for creating modern web applications. I specialize in React and Tailwind CSS.</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="mt-[26vh] w-full h-fit bg-black/70 backdrop-blur-lg">
+      <div className="flex flex-col items-left justify-center px-4 sm:px-[10%] lg:px-[20%]">
+        <div className="grid grid-cols-7 items-center">
+          <h1 className="col-span-7 text-4xl md:text-5xl xl:text-6xl font-bold text-white mt-32">Welcome, I'm Dawson.</h1>
+          <p className="col-span-7 md:col-span-4 text-lg md:text-xl text-white mt-5">Web developer dedicated to helping small businesses build a stronger online presence and stand out in today's digital landscape.</p>
+          <button className="col-span-7 md:col-span-3 md:justify-self-end flex group w-fit h-fit bg-white/80 text-black px-4 py-2 mt-5 rounded-full text-md sm:text-lg font-bold hover:bg-white transform hover:scale-[1.15] transition duration-200 ease-in-out">
+            Start a Project <img src="/arrow-black.svg" className="w-8 pl-1 transform group-hover:translate-x-3 transition duration-200 ease-in-out"/>
+          </button>
         </div>
-    )
+
+        <div className="flex flex-row w-full items-center justify-left mt-10">
+          <p className="text-sm sm:text-lg text-white/55 w-24 md:w-28">About Me</p>
+          <hr className="w-full border-t-1 border-white/15 mt-4 mb-3"/>
+        </div>
+
+        <div className="flex  flex-col md:flex-row space-x-10 pb-20">
+          <div className="bg-gradient-to-b from-neutral-900 to-black/90 rounded-xl border-white/15 border-1 text-white p-5 mt-10 w-full md:w-1/2">
+            <img src="/dawson.png" alt="" className="w-[40%] md:w-[40%] px-[5%] pt-[1%] rounded-lg bg-neutral-900" />
+            <p className="text-lg mt-4">I am a passionate developer with a love for creating modern web applications. I specialize in React and Tailwind CSS.</p>
+          </div>
+
+          <div className="bg-gradient-to-b from-neutral-900 to-black/90 rounded-xl border-white/15 border-1 text-white p-5 mt-10 w-full md:w-1/2">
+            <h2 className="text-3xl font-bold">About Me</h2>
+            <p className="text-lg mt-4">I am a passionate developer with a love for creating modern web applications. I specialize in React and Tailwind CSS.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,46 +1,46 @@
 export default function Nav() {
-    return (
-        <nav className="w-full h-fit fixed top-0 z-999">
-            <div className="bg-black/70 backdrop-blur-lg text-white p-4 mx-auto flex justify-center items-center w-fit mt-5 border-2 border-sky-950 rounded-full">
-                <div className="container mx-auto flex justify-center items-center">
-                    <img src="/Header-Logo.png" alt="Your Logo Here" className="h-12 pr-6 hidden md:block"/>
-                    <ul className="flex space-x-2 sm:space-x-5 text-lg font-[Helvetica] font-semibold items-center">
-                        <li>
-                            <div className="flex flex-col group">   
-                                <a href="/"><img src="home-icon-white.svg" alt="Home" className="w-8 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
-                                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-11 -mt-5 -mx-3 px-2">Home</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="flex flex-col group">   
-                                <a href="/about"><img src="about-icon-white.svg" alt="About" className="w-6 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
-                                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-12 -mt-5 -mx-4 px-2">About</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="flex flex-col group">   
-                                <a href="/projects"><img src="projects-icon-white.svg" alt="Projects" className="w-6 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
-                                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-12 -mt-5 -mx-6 px-2">Projects</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="flex flex-col group">   
-                                <a href="/services"><img src="services-icon.svg" alt="Services" className="w-6 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
-                                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-12 -mt-5 -mx-6 px-2">Services</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="flex flex-col group">   
-                                <a href="/contact"><img src="contact-icon-white.svg" alt="Contact" className="w-6 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
-                                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-12 -mt-5 -mx-6 px-2">Contact</div>
-                            </div>
-                        </li>
-                        <li>
-                            <button className="bg-white/80 text-black ml-3 sm:ml-6 px-2 sm:px-5 py-2 rounded-full text-[12px] sm:text-sm hover:bg-white transform hover:scale-[1.15] transition duration-200 ease-in-out">Free Quote</button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="w-full h-fit fixed top-0 z-999">
+      <div className="bg-black/70 backdrop-blur-lg text-white p-4 mx-auto flex justify-center items-center w-fit mt-5 border-2 border-sky-950 rounded-full">
+        <div className="container mx-auto flex justify-center items-center">
+          <img src="/Header-Logo.png" alt="Your Logo Here" className="h-12 pr-6 hidden md:block"/>
+          <ul className="flex space-x-2 sm:space-x-5 text-lg font-[Helvetica] font-semibold items-center">
+            <li>
+              <div className="flex flex-col group">   
+                <a href="/"><img src="home-icon-white.svg" alt="Home" className="w-8 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
+                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-11 -mt-5 -mx-3 px-2">Home</div>
+              </div>
+            </li>
+            <li>
+              <div className="flex flex-col group">   
+                <a href="/about"><img src="about-icon-white.svg" alt="About" className="w-6 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
+                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-12 -mt-5 -mx-4 px-2">About</div>
+              </div>
+            </li>
+            <li>
+              <div className="flex flex-col group">   
+                <a href="/projects"><img src="projects-icon-white.svg" alt="Projects" className="w-6 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
+                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-12 -mt-5 -mx-6 px-2">Projects</div>
+              </div>
+            </li>
+            <li>
+              <div className="flex flex-col group">   
+                <a href="/services"><img src="services-icon.svg" alt="Services" className="w-6 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
+                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-12 -mt-5 -mx-6 px-2">Services</div>
+              </div>
+            </li>
+            <li>
+              <div className="flex flex-col group">   
+                <a href="/contact"><img src="contact-icon-white.svg" alt="Contact" className="w-6 opacity-70 hover:opacity-100 transform hover:scale-[1.45] transition duration-200 ease-in-out"/></a>
+                <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 cursor-default rounded-md w-fit h-fit bg-sky-950 text-gray-200 text-sm translate-y-12 -mt-5 -mx-6 px-2">Contact</div>
+              </div>
+            </li>
+            <li>
+              <button className="bg-white/80 text-black ml-3 sm:ml-6 px-2 sm:px-5 py-2 rounded-full text-[12px] sm:text-sm hover:bg-white transform hover:scale-[1.15] transition duration-200 ease-in-out">Free Quote</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
