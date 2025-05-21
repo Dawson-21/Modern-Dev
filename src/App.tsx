3511094dbb96a4e0fac9b25d67a6a23c2dc84b24
Dawson Adams <dawson.adams121@gmail.com>
2025-05-21 02:59:37 -0500

    fix(style): Correct the sizing of page elements for larger screens.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
 import About from './components/About'
+import PageSizer from './components/PageSizer'
 
 function App() {
 
   return (
     <>
+      <PageSizer />
       <Nav />
       <Hero />
       <About />
